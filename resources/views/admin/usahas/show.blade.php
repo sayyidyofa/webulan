5780fe55c03c614f4b53d92b8f64ac56de14d1d1
Sayyid Yofa <sayyidyofa@gmail.com>
2021-01-28 20:22:35 +0700

    Finish complete usaha show

--- resources/views/admin/usahas/show.blade.php
+++ resources/views/admin/usahas/show.blade.php
@@ -13,82 +13,218 @@
                     {{ trans('global.back_to_list') }}
                 </a>
             </div>
-            <table class="table table-bordered table-striped">
-                <tbody>
-                    <tr>
-                        <th>
-                            {{ trans('cruds.usaha.fields.id') }}
-                        </th>
-                        <td>
-                            {{ $usaha->id }}
-                        </td>
-                    </tr>
-                    <tr>
-                        <th>
-                            {{ trans('cruds.usaha.fields.nama') }}
-                        </th>
-                        <td>
-                            {{ $usaha->nama }}
-                        </td>
-                    </tr>
-                    <tr>
-                        <th>
-                            {{ trans('cruds.usaha.fields.brand') }}
-                        </th>
-                        <td>
-                            {{ $usaha->brand }}
-                        </td>
-                    </tr>
-                    <tr>
-                        <th>
-                            {{ trans('cruds.usaha.fields.pengusaha') }}
-                        </th>
-                        <td>
-                            {{ $usaha->pengusaha->nama ?? '' }}
-                        </td>
-                    </tr>
-                    <tr>
-                        <th>
-                            {{ trans('cruds.usaha.fields.deskripsi') }}
-                        </th>
-                        <td>
-                            {{ $usaha->deskripsi }}
-                        </td>
-                    </tr>
-                    <tr>
-                        <th>
-                            {{ trans('cruds.usaha.fields.kategori') }}
-                        </th>
-                        <td>
-                            {{ $usaha->kategori }}
-                        </td>
-                    </tr>
-                    <tr>
-                        <th>
-                            {{ trans('cruds.usaha.fields.kontak') }}
-                        </th>
-                        <td>
-                            {{ $usaha->kontak }}
-                        </td>
-                    </tr>
-                    <tr>
-                        <th>
-                            {{ trans('cruds.usaha.fields.alamat_maps') }}
-                        </th>
-                        <td>
-                            {{ $usaha->alamat_maps }}
-                        </td>
-                    </tr>
-                    <tr>
-                        <th>
-                            {{ trans('cruds.usaha.fields.kegiatan') }}
-                        </th>
-                        <td>
-                            {{ $usaha->kegiatan }}
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
+            <div class="card">
+                <div class="card-header">Detail Usaha</div>
+                <div class="card-body">
+                    <table class="table table-bordered table-striped">
+                        <tbody>
+                        <tr>
+                            <th>
+                                {{ trans('cruds.usaha.fields.id') }}
+                            </th>
+                            <td>
+                                {{ $usaha->id }}
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>
+                                {{ trans('cruds.usaha.fields.nama') }}
+                            </th>
+                            <td>
+                                {{ $usaha->nama }}
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>
+                                {{ trans('cruds.usaha.fields.brand') }}
+                            </th>
+                            <td>
+                                {{ $usaha->brand }}
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>
+                                {{ trans('cruds.usaha.fields.pengusaha') }}
+                            </th>
+                            <td>
+                                {{ $usaha->pengusaha->nama ?? '' }}
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>
+                                {{ trans('cruds.usaha.fields.deskripsi') }}
+                            </th>
+                            <td>
+                                {{ $usaha->deskripsi }}
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>
+                                {{ trans('cruds.usaha.fields.kategori') }}
+                            </th>
+                            <td>
+                                {{ $usaha->kategori }}
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>
+                                {{ trans('cruds.usaha.fields.kontak') }}
+                            </th>
+                            <td>
+                                {{ $usaha->kontak }}
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>
+                                {{ trans('cruds.usaha.fields.alamat_maps') }}
+                            </th>
+                            <td>
+                                {{ $usaha->alamat_maps }}
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>
+                                {{ trans('cruds.usaha.fields.kegiatan') }}
+                            </th>
+                            <td>
+                                {{ $usaha->kegiatan }}
+                            </td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-header">Produk Unggulan Usaha</div>
+                <div class="card-body">
+                    @foreach($usaha->usahaProdukUnggulans as $produkUnggulan)
+                        <div class="card">
+                            <div class="card-header">Produk #{{ $loop->iteration }}</div>
+                            <div class="card-body">
+                                <table class="table table-bordered table-striped">
+                                    <tbody>
+                                        <tr>
+                                            <th>{{ trans('cruds.produkUnggulan.fields.nama') }}</th>
+                                            <td>{{ $produkUnggulan->nama ?? '' }}</td>
+                                        </tr>
+                                        <tr>
+                                            <th>{{ trans('cruds.produkUnggulan.fields.deskripsi') }}</th>
+                                            <td>{{ $produkUnggulan->deskripsi ?? '' }}</td>
+                                        </tr>
+                                        <tr>
+                                            <th>{{ trans('global.actions') }}</th>
+                                            <td>
+                                                @can('produk_unggulan_show')
+                                                    <a class="btn btn-xs btn-primary" href="{{ route('admin.produk-unggulans.show', $produkUnggulan->id) }}">
+                                                        {{ trans('global.view') }}
+                                                    </a>
+                                                @endcan
+
+                                                @can('produk_unggulan_edit')
+                                                    <a class="btn btn-xs btn-info" href="{{ route('admin.produk-unggulans.edit', $produkUnggulan->id) }}">
+                                                        {{ trans('global.edit') }}
+                                                    </a>
+                                                @endcan
+
+                                                @can('produk_unggulan_delete')
+                                                    <form action="{{ route('admin.produk-unggulans.destroy', $produkUnggulan->id) }}" method="POST" onsubmit="return confirm('{{ trans('global.areYouSure') }}');" style="display: inline-block;">
+                                                        <input type="hidden" name="_method" value="DELETE">
+                                                        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                                                        <input type="submit" class="btn btn-xs btn-danger" value="{{ trans('global.delete') }}">
+                                                    </form>
+                                                @endcan
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                                @foreach($produkUnggulan->produkUnggulanFotoProduks as $fotoProduk)
+                                    <div class="card">
+                                        <div class="card-header">Foto Produk #{{ $loop->parent->iteration }}</div>
+                                        <div class="card-body">
+                                            <div class="form-group">
+                                                @foreach($fotoProduk->foto as $key => $media)
+                                                    <a href="{{ $media->getUrl() }}" target="_blank" style="display: inline-block">
+                                                        <img src="{{ $media->getUrl('thumb') }}">
+                                                    </a>
+                                                @endforeach
+                                            </div>
+                                            <div class="form-group">
+                                                @can('foto_produk_show')
+                                                    <a class="btn btn-xs btn-primary" href="{{ route('admin.foto-produks.show', $fotoProduk->id) }}">
+                                                        {{ trans('global.view') }}
+                                                    </a>
+                                                @endcan
+
+                                                @can('foto_produk_edit')
+                                                    <a class="btn btn-xs btn-info" href="{{ route('admin.foto-produks.edit', $fotoProduk->id) }}">
+                                                        {{ trans('global.edit') }}
+                                                    </a>
+                                                @endcan
+
+                                                @can('foto_produk_delete')
+                                                    <form action="{{ route('admin.foto-produks.destroy', $fotoProduk->id) }}" method="POST" onsubmit="return confirm('{{ trans('global.areYouSure') }}');" style="display: inline-block;">
+                                                        <input type="hidden" name="_method" value="DELETE">
+                                                        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                                                        <input type="submit" class="btn btn-xs btn-danger" value="{{ trans('global.delete') }}">
+                                                    </form>
+                                                @endcan
+                                            </div>
+                                        </div>
+                                    </div>
+                                @endforeach
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-header">Media Sosial Usaha</div>
+                <div class="card-body">
+                    @foreach($usaha->usahaMediaSosials as $mediaSosial)
+                        <div class="card">
+                            <div class="card-header">Media Sosial #{{ $loop->iteration }}</div>
+                            <div class="card-body">
+                                <table class="table table-bordered table-striped">
+                                    <tbody>
+                                        <tr>
+                                            <th>{{ trans('cruds.mediaSosial.fields.link_accname') }}</th>
+                                            <td>{{ $mediaSosial->link_accname ?? '' }}</td>
+                                        </tr>
+                                        <tr>
+                                            <th>{{ trans('cruds.mediaSosial.fields.vendor') }}</th>
+                                            <td>{{ App\Models\MediaSosial::VENDOR_RADIO[$mediaSosial->vendor] ?? '' }}</td>
+                                        </tr>
+                                        <tr>
+                                            <th>{{ trans('global.actions') }}</th>
+                                            <td>
+                                                @can('media_sosial_show')
+                                                    <a class="btn btn-xs btn-primary" href="{{ route('admin.media-sosials.show', $mediaSosial->id) }}">
+                                                        {{ trans('global.view') }}
+                                                    </a>
+                                                @endcan
+
+                                                @can('media_sosial_edit')
+                                                    <a class="btn btn-xs btn-info" href="{{ route('admin.media-sosials.edit', $mediaSosial->id) }}">
+                                                        {{ trans('global.edit') }}
+                                                    </a>
+                                                @endcan
+
+                                                @can('media_sosial_delete')
+                                                    <form action="{{ route('admin.media-sosials.destroy', $mediaSosial->id) }}" method="POST" onsubmit="return confirm('{{ trans('global.areYouSure') }}');" style="display: inline-block;">
+                                                        <input type="hidden" name="_method" value="DELETE">
+                                                        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                                                        <input type="submit" class="btn btn-xs btn-danger" value="{{ trans('global.delete') }}">
+                                                    </form>
+                                                @endcan
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
+            </div>
             <div class="form-group">
                 <a class="btn btn-default" href="{{ route('admin.usahas.index') }}">
                     {{ trans('global.back_to_list') }}
